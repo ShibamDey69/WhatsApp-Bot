f678d10419300e091f6e79e2ce9d75ee07115194
Shibam Dey <123672103+NekoSenpai69@users.noreply.github.com>
2024-05-29 12:26:30 +0000

    More accurately lofi

--- src/commands/converter/tomp3.js
+++ src/commands/converter/tomp3.js
@@ -1,7 +1,7 @@
-import converter from '../../utils/converter.js'
+import converter from "../../utils/converter.js";
 export default {
   name: "toaudio",
-  alias: ["tomp3","adch"],
+  alias: ["tomp3", "adch"],
   desc: "Converts a video or audio to different version of audio",
   category: "converter",
   usage: `tomp3 <args> qoute/tag the video/audio`,
@@ -21,7 +21,7 @@ export default {
           M,
         );
       }
-      if(M.quoted.mtype !== "audio" && M.quoted.mtype !== "video") {
+      if (M.quoted.mtype !== "audio" && M.quoted.mtype !== "video") {
         return await Neko.sendTextMessage(
           M.from,
           "Please quote the video or audio you want to convert to audio.",
@@ -39,37 +39,50 @@ export default {
         return await Neko.sendTextMessage(
           M.from,
           "Available audio types: normal, nightcore, bassboost, lofi",
-          M
-        )
+          M,
+        );
       }
-      const media = await Neko.downloadMediaContent(Neko,M.quoted);
+      const media = await Neko.downloadMediaContent(Neko, M.quoted);
       let audio;
       switch (M.args.toLowerCase()) {
-         case 'normal':
-          audio = await converter(media.data, 'mp3')
-            break;
-        case 'nightcore':
-          audio = await converter(media.data, 'mp3',["-af","asetrate=44100*1.15,aresample=44100,atempo=1.15"])
+        case "normal":
+          audio = await converter(media.data, "mp3");
           break;
-        case 'bassboost':
-          audio = await converter(media.data, 'mp3',["-af","bass=g=30, dynaudnorm=f=150"])
+        case "nightcore":
+          audio = await converter(media.data, "mp3", [
+            "-af",
+            "asetrate=44100*1.15,aresample=44100,atempo=1.15",
+          ]);
           break;
-        case 'lofi':
-          audio = await converter(media.data, 'mp3',["-af","aformat=sample_fmts=s16:sample_rates=22050, lowpass=f=1200, highpass=f=100, afftdn=nf=-30, aecho=0.8:0.88:60:0.36, atempo=0.85, equalizer=f=300:t=q:w=1.5:g=5, equalizer=f=1000:t=q:w=1.5:g=3"])
-          break
-         default:
-         await Neko.sendTextMessage(M.from, "Invalid audio type. Please use --list to see the available audio types.",M)
-            break;
+        case "bassboost":
+          audio = await converter(media.data, "mp3", [
+            "-af",
+            "bass=g=30, dynaudnorm=f=150",
+          ]);
+          break;
+        case "lofi":
+          audio = await converter(media.data, "mp3", [
+            "-af",
+            "aformat=sample_fmts=s16:sample_rates=22050, lowpass=f=1200, highpass=f=100, afftdn=nf=-65, aecho=0.8:0.88:60:0.1, atempo=0.88, equalizer=f=300:t=q:w=1.5:g=5, equalizer=f=1000:t=q:w=1.5:g=3",
+          ]);
+          break;
+        default:
+          await Neko.sendTextMessage(
+            M.from,
+            "Invalid audio type. Please use --list to see the available audio types.",
+            M,
+          );
+          break;
       }
-      await Neko.sendMessage(M.from, { audio: audio, mimetype: "audio/mpeg" }, { quoted: M });
+      await Neko.sendMessage(
+        M.from,
+        { audio: audio, mimetype: "audio/mpeg" },
+        { quoted: M },
+      );
       return;
     } catch (error) {
       console.log(error);
       throw new Error(error);
     }
-  }
-}
-
-
-
-
+  },
+};
