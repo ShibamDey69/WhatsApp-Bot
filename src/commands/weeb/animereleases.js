8edf960097807d29a49cd0dfa68b2ad4005874c3
nekosenpai269 <nekosenpai269@gmail.com>
2024-08-29 12:16:05 +0000

    sorted

--- src/commands/weeb/animereleases.js
+++ src/commands/weeb/animereleases.js
@@ -15,17 +15,25 @@ export default {
   cooldown: 10,
   run: async (Neko, M) => {
     try {
-      const data = await animeair();
+      const day = new Date();
+      const startDay = day.setHours(0, 0, 0, 0);
+      const endDay = day.setHours(23, 59, 59, 999);
+      const startDayMs = Math.floor(startDay / 1000);
+      const endDayMs = Math.floor(endDay / 1000);
+
+      const data = await animeair(startDayMs, endDayMs);
       if (!data) {
         return await Neko.sendTextMessage(M.from, "No anime releases found", M);
       }
-      data.forEach(async (anime) => {
-        const { id, episode, media, airingAt } = anime;
-        const { title, description } = media;
-        const message = `*Id:* ${id}\n*Title:* ${title.romaji}\n*Episode:* ${episode}\n*Airing in:* ${new Date(airingAt * 1000 + 5 * 60 * 60 * 1000 + 30 * 60 * 1000).toLocaleString()}\n*Description:* ${description}\n `;
-          
-            await Neko.sendTextMessage(M.from, message, M);
-      });
+      data
+        .sort((resA, resB) => resA.airingAt - resB.airingAt)
+        .forEach(async (anime) => {
+          const { id, episode, media, airingAt } = anime;
+          const { title, description } = media;
+          const message = `*Id:* ${id}\n*Title:* ${title.romaji}\n*Episode:* ${episode}\n*Airing in:* ${new Date(airingAt * 1000 + 5 * 60 * 60 * 1000 + 30 * 60 * 1000).toLocaleString()}\n*Description:* ${description ?? "null"}\n `;
+
+          await Neko.sendTextMessage(M.from, message, M);
+        });
 
       // await Neko.sendImageMessage(M.from, image_url, M,message);
     } catch (error) {
